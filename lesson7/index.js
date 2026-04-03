430d76d72da011f864d33f4cfd820ce9e506f932
AyubovAbdulaziz <abdulazizayubov@gmail.com>
2026-04-03 10:39:03 +0500

    git buyruqlari darsi

---
+++ lesson7/index.js
@@ -0,0 +1,25 @@
+console.log("GitHub")
+
+//  git init
+
+// git remote add origin httpsL://repozitory-manzili
+
+// git add .
+
+// git commit -m "commentariya yoziladi"
+
+// git push origin master
+
+// ====================================
+
+// qo'shimcha so'rab qolsa:
+// git config --global user.email "abdulazizayubov@gmail.com"
+// git config --global user.name "AyubovAbdulaziz"
+
+// ====================================
+
+// Doim yangi code yozsak 3ta git buyrugini doim ishlatishimiz shart.
+
+// git add .
+// git commit -m "komentariya yozing"
+// git push origin master
