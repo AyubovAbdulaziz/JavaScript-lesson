18dcd2663acfbb148c041167ee60ade3aac4ea55
AyubovAbdulaziz <abdulazizayubov@gmail.com>
2026-04-22 09:51:02 +0500

    Object

---
+++ lesson15/index.js
@@ -0,0 +1,36 @@
+// let a = 1
+// let b = 2
+// let c = 3
+// function eshmat(r1, r2, r3) {
+//     let summa = r1, r2, r3;
+//     return summa;
+// }
+// let sum = eshmat(a, b, c);
+
+// console.log(sum / 2);
+
+// OBJECT - {}
+
+// 1. {}
+// 2. Object()
+
+let obj = {
+    ism: "Abdulaziz",
+    familiya: "Ayubov",
+    yosh: 13,
+    isGoSchool: true,
+    manzil: "Namangan shahar",
+    telRaqam:"+998 99 250 65 61"
+};
+
+// console.log(obj);
+
+// 1. Static usul
+console.log(obj.familiya, obj.ism);
+
+// 2. Dinamic usul
+console.log(obj["familiya"], obj["ism"]);
+
+
+
+
